064095f778b15612a8c7bcf88faec1a522f76915
matheusouzagoncalves <mateusgoncalves635@gmail.com>
2025-12-07 19:06:15 -0300

    Implementação de Contas - Ajustes 2

---
+++ src/main/java/com/example/loginauthapi/domain/cadastro/Recorrencia.java
@@ -0,0 +1,5 @@
+package com.example.loginauthapi.domain.cadastro;
+
+public enum Recorrencia {
+    UNICA, SEMANAL, MENSAL, TRIMESTRAL, SEMESTRAL, ANUAL
+}
---
+++ src/main/java/com/example/loginauthapi/domain/cadastro/TipoConta.java
@@ -0,0 +1,6 @@
+package com.example.loginauthapi.domain.cadastro;
+
+// 🚨 Tipos de Enums para Recorrência e Tipo
+ public enum TipoConta {
+    RECEITA, DESPESA
+}
